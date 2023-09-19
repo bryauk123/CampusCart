0512295f4e77e8f7a41d18efc674ea1468890b58
bryauk123 <91648730+bryauk123@users.noreply.github.com>
2023-09-19 02:33:50 -0700

    Initial Commit

---
+++ CampusCart/CampusCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusCartApp.swift
+//  CampusCart
+//
+//  Created by Bryan Apodaca on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CampusCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
